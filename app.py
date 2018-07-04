7223f42251cdba29fc7c8c420be04d1fd77454b9
Marcel Hild <hild@b4mad.net>
2018-07-04 12:28:52 +0200

    wrap in flask app

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
+import os
+import pickle
 import clustering
+from flask import Flask
 
-cluster = clustering.cluster()
-print(cluster)
+app = Flask(__name__)
+cluster = None
+if os.path.isfile('cluster.p'):
+    cluster = pickle.load(open('cluster.p', 'rb'))
+
+
+@app.route("/")
+def index():
+    global cluster
+
+    if cluster is not None:
+        return "<pre>%s</pre>" % cluster.to_string()
+    else:
+        return "hit /update to create the clustering"
+
+@app.route("/update")
+def update():
+    global cluster
+
+    cluster = clustering.cluster()
+    pickle.dump(cluster, open('cluster.p', 'wb'))
+
+    return "Clustering updated"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
