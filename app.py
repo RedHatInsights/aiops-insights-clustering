ed43327362326f0be6d90c3baabb714e2e021fac
Jesse Jaggars <jjaggars@redhat.com>
2018-07-06 14:12:51 -0500

    adding sync endpoint

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 import json
 import os
 import pickle
+from concurrent.futures import ProcessPoolExecutor
 from flask import Flask
 import clustering
 import storage
+import sync
 
 app = Flask(__name__)
 
@@ -15,6 +21,14 @@ def index(date):
         return "<pre>%s</pre>" % CLUSTERS[date].to_string()
     else:
         return json.dumps(list(storage.available()))
+
+
+@app.route("/sync")
+def sync_endpoint():
+    with ProcessPoolExecutor(max_workers=1) as executor:
+        f = executor.submit(sync.sync)
+        logging.info("Running sync job %s", f)
+    return "OK"
 
 
 if __name__ == "__main__":
