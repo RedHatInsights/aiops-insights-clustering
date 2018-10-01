2a67bf3f27fa16dd4e297d21c090de66ab2e3600
Justin Chen <juschen@redhat.com>
2018-10-01 14:01:20 -0400

    Stores kmeans parameters and results in ceph. Updated ceph.write() to either store locally or in s3 and a method stub forceph.read()

--- storage/ceph.py
+++ storage/ceph.py
@@ -1,15 +1,22 @@
 import pyarrow.parquet as pq
-import s3fs, datetime, os
+import s3fs, datetime, os, s3, local
 
 (ceph_key, ceph_secret, ceph_endpoint) = (os.getenv('CEPH_KEY'), os.getenv('CEPH_SECRET'), os.getenv('CEPH_ENDPOINT'))
 client_kwargs = {'endpoint_url': ceph_endpoint}
 
 
 def get_dataset(key):
-    s3 = s3fs.S3FileSystem(secret=ceph_secret, key=ceph_key, client_kwargs=client_kwargs)
-    return pq.ParquetDataset(key, filesystem=s3).read_pandas().to_pandas()
+    filesystem = s3fs.S3FileSystem(secret=ceph_secret, key=ceph_key, client_kwargs=client_kwargs)
+    return pq.ParquetDataset(key, filesystem=filesystem).read_pandas().to_pandas()
 
 
-def write(data):
-    s3 = s3fs.S3FileSystem(secret=ceph_secret, key=ceph_key, client_kwargs=client_kwargs)
-    s3.write(data, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+def write(data, use_local=True):
+	date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	if use_local:
+		local.write(data, date)
+	else:
+	    s3.write(data, date)
+
+
+def read(date, use_local=True):
+	raise NotImplementedError
