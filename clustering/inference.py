3094a947d63facda99cb028003aee003de1c62b2
sanjay <sanjay@localhost.localdomain>
2018-12-12 21:49:44 -0500

    Cleaning up package

--- clustering/inference.py
+++ clustering/inference.py
@@ -1,44 +1,52 @@
 import pandas as pd
 import numpy as np
 
-import train
+from .train import Cluster
 
 '''
 Save n_components in pca object and get rid of get_pca_ncomponents
 Add tests:
-	???
+    ???
 Ensure any df going to predict goes through preprocess
 '''
 
-class Inference(train.Cluster):
-	def __init__(self, pd_rules, categorical_cols=None, drop_cols=None):
-		super().__init__(pd_rules)
+class Inference(Cluster):
+    def __init__(self, pd_rules, index_cols=None, \
+                 categorical_cols=None, drop_cols=None,
+                 models_dict=None):
+        
+        super().__init__(pd_rules, index_cols=index_cols, categorical_cols=categorical_cols, drop_cols=drop_cols)
 
-		self.data = pd_rules
+        super().preprocess()
 
-		self.data_preprocess = self.preprocess(self.data, categorical_cols=categorical_cols, drop_cols=drop_cols)
+        self.models_dict = models_dict
+        self.load_models()
 
-	def load_models(self, prepca_scaler_filename, pca_filename, postpca_scaler_filename, cluster_filename):
-		self.prepca_scaler = self.load_model(prepca_scaler_filename)
-		self.pca = self.load_model(pca_filename)
-		self.postpca_scaler = self.load_model(postpca_scaler_filename)
-		self.kmeans_optimal = self.load_model(cluster_filename)
+    def load_models(self):
+        if self.models_dict is None:
+            raise AttributeError("Please specify pre-trained models to load")
 
-	def preprocess_prepare_for_clustering(self):
-		df_prepca_scaled = self.prepca_scaler.transform(self.data_preprocess)		
-		
-		df_pca = pd.DataFrame(self.pca.transform(df_prepca_scaled))
-		df_pca = df_pca.iloc[:,:self.pca.n_components]
+        self.prepca_scaler = self.models_dict['prepca_scaler']
+        self.pca = self.models_dict['pca']
+        self.postpca_scaler = self.models_dict['postpca_scaler']
+        self.kmeans_optimal = self.models_dict['kmeans']
 
-		df_postpca_scaled = self.postpca_scaler.transform(df_pca)
+    def preprocess_prepare_for_clustering(self):
+        df_prepca_scaled = self.prepca_scaler.transform(self.data)       
+        
+        df_pca = pd.DataFrame(self.pca.transform(df_prepca_scaled)).iloc[:,:self.pca.n_components]
 
-		self.data_for_kmeans = df_postpca_scaled
+        df_postpca_scaled = self.postpca_scaler.transform(df_pca)
 
-	def predict(self):		
-		'''Remove df as argument since don't want user to input any other df
-		'''
-		cl = self.kmeans_optimal.predict(self.data_for_kmeans)
+        self.data_for_kmeans = df_postpca_scaled
 
-		cl_dict = dict(zip(self.data.index, cl))
-		
-		return cl_dict
+    def predict(self):      
+        '''Remove df as argument since don't want user to input any other df
+        '''
+        self.preprocess_prepare_for_clustering()
+
+        cl = self.kmeans_optimal.predict(self.data_for_kmeans)
+
+        cl_dict = dict(zip(self.data.index, cl))
+        
+        return cl_dict
