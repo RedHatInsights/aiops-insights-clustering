e667e99427942c962f622d52fdb0dc80c4df3b05
Hema Veeradhi <hemav@bu.edu>
2019-03-07 16:31:11 -0500

    Update __init__.py
    Added the function calculate_metrics() to compare the behavior/performance of the clusters obtained for the two days.
    
    Metrics such as mutual information score and fowlkes mallows score are used to measure the similarity of two clusterings for the same data set.

--- metric_tracking/__init__.py
+++ metric_tracking/__init__.py
@@ -2,6 +2,7 @@ import logging
 import storage
 
 import mlflow
+from sklearn import metrics
 
 # for a more sophisticated method have a look at
 #  https://gitlab.cee.redhat.com/mcliffor/insights_clustering/blob/master/stability_score.py
@@ -15,12 +16,13 @@ def do_tracking():
         mlflow.log_param("Date A", date_a)
         mlflow.log_param("Date B", date_b)
         calculate_score(date_a, date_b)
+        calculate_metrics(date_a,date_b)
         mlflow.end_run()
-
+    
 def calculate_score(date_a, date_b):
     cluster_a = storage.read(date_a)
     cluster_b = storage.read(date_b)
-
+    
     inverse_cluster_a = invert_cluster(cluster_a)
     inverse_cluster_b = invert_cluster(cluster_b)
 
@@ -40,7 +42,8 @@ def calculate_score(date_a, date_b):
 
         median_number_ids_in_cluster = (len(systems_a) + len(systems_b)) / 2.0
         score = number_ids_in_both / median_number_ids_in_cluster
-
+        
+        mlflow.log_metric(("number_of_ids_in_both_days for cluster_%s" cluster_id),number_ids_in_both)
         mlflow.log_metric(("cluster_stability_%s" % cluster_id), score)
         logging.info("cluster_id %i - stability score %f" % (cluster_id, score))
 
@@ -51,3 +54,37 @@ def invert_cluster(cluster):
         new_cluster[cluster_id].append(system_id)
     return new_cluster
 
+def calculate_metrics(date_a,date_b):
+    cluster_a = storage.read(date_a)
+    cluster_b = storage.read(date_b)
+    
+    #Find the list of common system ids in both the days
+    sys_ids_a = find_sys_ids(cluster_a)
+    sys_ids_b = find_sys_ids(cluster_b)
+    sys_ids_in_both = list(set(sys_ids_a).intersection(set(sys_ids_b)))
+    num_sys_ids_in_both = len(set(sys_ids_a).intersection(set(sys_ids_b)))
+    
+    #Find the cluster labels/ids for the common system ids in both days
+    labels_a, labels_b = find_labels(sys_ids_in_both,cluster_a,cluster_b)
+    
+    #Find the performance metrics such as mutual information score, fowlkes mallows score
+    mutual_info_score = metrics.mutual_info_score(labels_a, labels_b)
+    fowlkes_mallows_score = metrics.fowlkes_mallows_score(labels_a, labels_b)
+    adjusted_rand_score = metrics.adjusted_rand_score(labels_a, labels_b)
+    
+    mlflow.log_metric("total_number_of_ids_in_both_days",num_sys_ids_in_both)
+    mlflow.log_metric("mutual_info_score",mutual_info_score)
+    mlflow.log_metric("fowlkes_mallows_score",fowlkes_mallows_score)
+    mlflow.log_metric("adjusted_rand_score",adjusted_rand_score)
+    
+def find_sys_ids(cluster):
+    sys_ids = sorted(list(cluster.keys())
+    return sys_ids
+
+def find_new_labels(common_sys_ids,cluster_1,cluster_2):
+    cluster_labels_1 = []
+    cluster_labels_2 = []
+    for system_id in sorted(common_sys_ids):
+        cluster_labels_1.append(cluster_1[system_id])
+        cluster_labels_2.append(cluster_2[system_id])
+    return cluster_labels_1, cluster_labels_2
